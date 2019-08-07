624164dac152436d8573f5986508d34fe4a2f393
Manishankar <shankar.bandaru777@gmail.com>
2019-08-07 15:59:08 +0530

    Module for controlling human made issue like quick creation of contacts,products etc

---
+++ a2nsoft_essentials/__manifest__.py
@@ -0,0 +1,22 @@
+
+{
+    'name': 'a2NSoft Essentials',
+    'version': '1.0',
+    'author': 'Mani Shankar',
+    'category': 'Productivity',
+    'description': """
+        This module helps to do avoid quick creation of the products, customers, vendors, analytical accounts 
+    """,
+
+    'summary': 'This module helps to avoid Human Mistakes',
+    'depends': ['base', 'sale','account','purchase'],
+
+    'data': [
+		'views/custom_sales_order.xml'
+
+
+    ],
+
+    'installable': True,
+    'auto_install': False,
+}
