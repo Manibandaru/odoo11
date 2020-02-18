918263ed3641c00b877367337ffd56ed590fb443
Manishankar <shankar.bandaru777@gmail.com>
2020-02-18 12:50:57 +0530

    Manufacturing modules added

---
+++ a2nsoft_install/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'a2NSoft Installation',
+    'version': '1.0',
+    'author': 'Mani Shankar',
+    'category': 'Productivity',
+    'description': """
+        This module helps all the a2nsoft modules in one shot
+    """,
+
+    'summary': 'This module helps to avoid Human Mistakes',
+    'depends': ['base','crm','project', 'sale','account','purchase','product_margin','account_asset','a2nsoft_essentials','account_reports_xlsx','account_standard_report','backend_theme_v11',
+                'check_printing','custom_account','custom_hr','google_map','mail_debrand','odoo_web_login','pos_ticket','report_xlsx',
+                 'vat_report','web_debranding'],
+
+
+
+    'installable': True,
+    'auto_install': False,
+}
